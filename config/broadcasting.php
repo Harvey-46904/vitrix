9a89d14d2a6299907fbafe9af1e533508646a026
Harvey-46904 <harveympv@hotmail.com>
2025-02-27 02:03:31 -0500

    cambio pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,18 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'https',  // Usar 'https' para WebSockets seguros
                 'useTLS' => true,
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
-            ]
-        ], 'client_options' => [
-            'verify' => true
+            ],
         ],
         
         'ably' => [
