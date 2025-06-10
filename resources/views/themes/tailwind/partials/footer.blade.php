19163947db5af02798d772cc2ffe31aa3c9a75e5
Harvey-46904 <harveympv@hotmail.com>
2025-06-10 11:03:17 -0500

    sitemax.xmle

--- resources/views/themes/tailwind/partials/footer.blade.php
+++ resources/views/themes/tailwind/partials/footer.blade.php
@@ -86,7 +86,7 @@
                         </li>
 
                         <li>
-                            <a href="{{route('sitemap')}}" class="relative inline-block text-black group">
+                            <a href="/sitemap.xml" target="_blank" class="relative inline-block text-black group">
                                 <span
                                     class="absolute bottom-0 w-full transition duration-150 ease-out transform -translate-y-1 border-b border-black opacity-0 group-hover:opacity-100 group-hover:translate-y-1"></span>
                                 <span class="text-white">Sitemap</span>
