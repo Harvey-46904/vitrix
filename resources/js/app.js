c12f293380cf1751d551d2807f5b324ea3b73c13
Harvey-46904 <harveympv@hotmail.com>
2025-07-26 16:02:30 -0500

    cluestr quit

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,14 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: process.env.MIX_REVERB_APP_KEY,
-    cluster: process.env.MIX_REVERB_APP_CLUSTER,
-    wsHost: process.env.MIX_REVERB_URL,
-    wsPort: process.env.MIX_REVERB_PORT,
-    wssPort: process.env.MIX_REVERB_PORT,
-    forceTLS: (process.env.MIX_REVERB_SCHEME ?? 'https') === 'https',
-    path: process.env.MIX_REVERB_PATH,
-    enabledTransports: ['ws', 'wss'],
+  broadcaster: 'reverb',
+  key: process.env.MIX_REVERB_APP_KEY,
+  wsHost: process.env.MIX_REVERB_URL,   // `vitrix.io`
+  wsPort: process.env.MIX_REVERB_PORT,  // `6001` *siempre en HTTPS via proxy en 443*
+  wssPort: process.env.MIX_REVERB_PORT,
+  forceTLS: (process.env.MIX_REVERB_SCHEME ?? 'https') === 'https',
+  path: process.env.MIX_REVERB_PATH,     // `/app`
+  enabledTransports: ['ws', 'wss'],
 });
 
 echo.channel('chat')
