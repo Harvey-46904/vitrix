1c78897d3ebcd56af89ddfe76e91b709de8ae479
Harvey-46904 <harveympv@hotmail.com>
2025-07-26 15:10:39 -0500

    nueva configuaracion de rebert con url

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ const echo = new Echo({
     broadcaster: 'reverb',
     key: process.env.MIX_REVERB_APP_KEY,
     cluster: process.env.MIX_REVERB_APP_CLUSTER,
-    wsHost: process.env.MIX_REVERB_HOST,
+    wsHost: process.env.MIX_REVERB_URL,
     wsPort: process.env.MIX_REVERB_PORT,
     wssPort: process.env.MIX_REVERB_PORT,
     forceTLS: (process.env.MIX_REVERB_SCHEME ?? 'https') === 'https',
